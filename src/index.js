8c6e4c35b7e9feaeabe5c320fafca1036634ce7c
praveendwivedi <praveen240903@gmail.com>
2025-01-12 00:03:04 +0530

    some bug fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+    path:"./.env"
+});
 
+import connectToDb from "./db/connectToDb.js";
+import { db_name } from "./constant.js";
 
+connectToDb(db_name)
+.then(()=>{
+    console.log("now we can connect listen on some port");
+    
+})
+.catch(error => console.log("we got some error in connecting to database"))
