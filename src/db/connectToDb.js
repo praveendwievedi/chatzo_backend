44f5a94cfa5f4054849cc9a633fb8c6ce0239814
praveendwivedi <praveen240903@gmail.com>
2025-01-12 00:02:29 +0530

    created a async fuction to connect to database

--- src/db/connectToDb.js
+++ src/db/connectToDb.js
@@ -1,7 +1,15 @@
 import mongoose  from "mongoose";
 
 const connectToDb = async(db_name)=>{
-    const connectionInstances=await mongoose.connect(`process.env.MONGO_URL/${db_name}`);
+    try {
+        const connectionInstances=await mongoose.connect(`${process.env.MONGO_URL}/${db_name}`);
+        console.log("Database connected successfully to host : ",
+                connectionInstances.connection.host);
+    } catch (error) {
+        console.log("Error connecting to databse",error.message);
+        
+    }
+    
 }
 
 export default connectToDb
