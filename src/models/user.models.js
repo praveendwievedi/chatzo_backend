3aed99a4d47d213d957971ca8b92c2d54f400780
PD <praveen240903@gmail.com>
2025-01-12 22:56:08 +0530

    add user data model

---
+++ src/models/user.models.js
@@ -0,0 +1,75 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema=new Schema({
+    userName:{
+        type:String,
+        required:true,
+        unique:true,
+        toLowerCse:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        // toLowerCse:true,
+        trim:true,
+        // index:true
+    },
+    fullName:{
+        type:String,
+        required:true,
+        // unique:true,
+        // toLowerCse:true,
+        trim:true,
+        // index:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true,
+        index:true
+    },
+    password:{
+        type:String,
+        required:[true,"password required"],
+    },
+    bio:{
+        type:String,
+    },
+    avatar:{
+        type:String,
+        // default:
+    },
+    friends:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password"))return next();
+
+    this.password=await bcrypt.hash(this.password,12)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            userName:this.userName,
+            email:this.email,
+        },
+        process.env.JWT_SECRET
+    )
+}
+
+export const User=new mongoose.model("User",userSchema)
+
